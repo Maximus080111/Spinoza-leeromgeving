34cb3b432e5e7b17b7b57fb5bb3951861066efe4
Maximus080111 <overbeekmax@gmail.com>
2024-11-25 13:09:46 +0100

    Add Google authentication support with Socialite integration

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GoogleController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function googleCallback()
+    {
+        try {
+
+            $user = Socialite::driver('google')->user();
+
+            $current_user = User::where('google_id', $user->id)->first();
+
+            if($current_user){
+
+                Auth::login($current_user);
+
+                return redirect()->intended('dashboard');
+
+            }else{
+                $newUser = User::updateOrCreate(['email' => $user->email],[
+                    'name' => $user->name,
+                    'google_id'=> $user->id,
+                    'password' => encrypt('123456dummy')
+                ]);
+
+                Auth::login($newUser);
+
+                return redirect()->intended('dashboard');
+            }
+
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
