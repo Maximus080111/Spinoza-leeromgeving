6c3986d7e8ffdb8492e2d989fb6029bc5f4540e2
Melvin van Vliet <melvinvanvliet@hotmail.com>
2024-11-27 14:10:08 +0100

    migration gemaakt voor alle data die we nodig hebben.

---
+++ database/migrations/2024_11_27_130748_create_question4_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('question4', function (Blueprint $table) {
+            $table->id();
+            $table->string('sentence');
+            $table->foreignId('theme_id')->references('id')->on('theme')->onDelete('cascade');
+            $table->foreignId('lesson_id')->references('id')->on('lesson')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('question4');
+    }
+};
